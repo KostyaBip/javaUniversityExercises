fc05b8ca6e43258a7ea351db4a62baaa7da69600
Konstantin <bykovkonstantin@gmail.com>
2023-09-08 19:50:02 +0300

    Practice number 1

--- src/ru/mirea/it/ivt/Main.java
+++ src/ru/mirea/it/ivt/Main.java
@@ -1,20 +1,64 @@
 package ru.mirea.it.ivt;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {/*
-        String s = "Hello world!";
-        System.out.println(s);
-        User user_1 = new User();
-        User user_2 = new User("Programmer", "EasyPass");
-        System.out.println("Number of users: " + User.NumberOfUsers);
-        System.out.println("Name: " + user_1.getUsername() + "; Password: " + user_1.getPassword());
-        System.out.println("Name: " + user_2.getUsername() + "; Password: " + user_2.getPassword());*/
-        User[] Users = {new User("Maria", "Pass"),
-                new User("Just", "FindIt"), new User()};
-        var i = 0;
-        while( i < Users.length){
-            System.out.println(Users[i]);
+    public static void Exercise_1_3(){
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int sumOfElem = 0;
+        float averageValOfElem;
+        for(int i : array){
+            sumOfElem += i;
+        }
+        averageValOfElem = (float)sumOfElem/ array.length;
+        System.out.print("Sum of element of array is " + sumOfElem +
+                "\nAverage value of elements of array is " + averageValOfElem);
+    }
+    public static void Exercise_1_4(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter values of array separated with space: ");
+        String valuesOfArray = scan.nextLine();
+        String[] splitedValues = valuesOfArray.split(" ");
+        int i = 0;
+        int max = Integer.parseInt(splitedValues[0]);
+        int min = max;
+        int sumOfElem = 0;
+        int[] array = new int[splitedValues.length];
+        while(i<splitedValues.length){
+            array[i] = Integer.parseInt(splitedValues[i]);
+            sumOfElem+=array[i];
+            max = max < array[i] ? array[i] : max;
+            min = min > array[i] ? array[i] : min;
             i++;
         }
+        System.out.print("Sum of element of array is " + sumOfElem +
+                "\nMax value of element of array is " + max +
+                "\nMin value of element of array is " + min);
+    }
+    public static void Exercise_1_5(){
+        //
+    }
+    public static void Exercise_1_6(){
+        for(int i = 0; i < 10; i++) {
+            System.out.print(1.0/(i+1) == 1/(i+1) ? 1/(i+1) + ", ": String.format("%.4f, ", 1.0/(i+1)));
+        }
+    }
+    public static int Exercise_1_7(int num){
+        int factorial = num;
+        while(num > 2){
+            factorial *= num-1;
+            num--;
+        }
+        return factorial;
+    }
+    public static void Exercise_1_8(){
+
+    }
+    public static void main(String[] args) {
+        // Exercise_1_7
+        /*System.out.print(Exercise_1_7(5));*/
+
+        // Exercise_1_5
+        /*for(String s : args){
+            System.out.println(s);
+        }*/
     }
 }
